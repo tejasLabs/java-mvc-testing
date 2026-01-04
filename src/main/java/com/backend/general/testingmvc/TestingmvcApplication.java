b10ac81e03ab312867149dc00cef51b59e6e8a50
Tejas <94tejas@gmail.com>
2026-01-04 15:10:16 +0530

    Project Initialized.

---
+++ src/main/java/com/backend/general/testingmvc/TestingmvcApplication.java
@@ -0,0 +1,13 @@
+package com.backend.general.testingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/general/testingmvc/TestingmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.general.testingmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
